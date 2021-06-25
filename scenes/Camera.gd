8dd6a4c5c5c36305474c52bcd9e3102527950c8f
Aravind-Sundararajan <aravind.sundararajan@protonmail.com>
2021-06-25 16:09:21 -0400

    cleaned up the ui, changed resolution

--- scenes/Camera.gd
+++ scenes/Camera.gd
@@ -1,9 +1,9 @@
 extends Camera
 var time_passed : float = 0.0 
+onready var dozer = get_parent()
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,4 +13,5 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	time_passed += delta
-	transform.origin = Vector3(0,10*sin(time_passed/TAU),0)
+	transform.origin = Vector3(-30,20*sin(time_passed/TAU),-30)
+	look_at(dozer.transform.origin, Vector3(0,1,0))
